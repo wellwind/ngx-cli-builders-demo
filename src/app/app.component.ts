a9435e092b953a783497547875a0fd0f0cc34a72
Mike Huang <wellwind@mail2000.com.tw>
2021-10-23 12:25:49 +0800

    加入簡易初始內容

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ngx-cli-builders-demo';
+
+  files$ = this.httpClient
+    .get<{articles: string[]}>('./assets/posts.json')
+    .pipe(
+      map(result => result.articles)
+    );
+
+  constructor(private httpClient: HttpClient) {
+  }
+
 }
